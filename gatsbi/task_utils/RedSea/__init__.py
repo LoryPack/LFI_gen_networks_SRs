8a8355a03cd8edf0de9854d2f93db15be27faa49
Lorenzo Pacchiardi <lorenzo.pacchiardi@stats.ox.ac.uk>
2024-02-15 11:19:03 +0000

    Remove Red Sea experiment

--- gatsbi/task_utils/RedSea/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dataloader import get_dataloader
-from .networks import RedSeaGenerator as Generator
-from .prior import SpatialConditionalExtremesPrior as Prior
-from .simulator import SpatialConditionalExtremesSim as Simulator
-
